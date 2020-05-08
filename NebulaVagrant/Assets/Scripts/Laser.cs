0871f964ecd5871cbd6262ea1c5fa5f09f3320cd
theKasra <thekasra@outlook.com>
2020-05-08 05:53:02 +0430

    Laser teleport and terminate added
    Note: teleport needs a fix.

--- NebulaVagrant/Assets/Scripts/Laser.cs
+++ NebulaVagrant/Assets/Scripts/Laser.cs
@@ -6,6 +6,11 @@ public class Laser : MonoBehaviour
 {
 
     [SerializeField] float laserSpeed;
+    [SerializeField] float minXTeleportBound;
+    [SerializeField] float maxXTeleportBound;
+    [SerializeField] float minYTeleportBound;
+    [SerializeField] float maxYTeleportBound;
+    [SerializeField] float destroyTime;
 
     Rigidbody2D rb2d;
 
@@ -14,17 +19,40 @@ public class Laser : MonoBehaviour
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        StartCoroutine(Terminate());
     }
 
     // Update is called once per frame
     void Update()
     {
         Projectile();
+        Teleport();
     }
 
     // The onward movement of laser projectile
     private void Projectile()
     {
         rb2d.velocity = transform.up * Time.deltaTime * laserSpeed;
+    }
+
+    // Teleports the laser if it's moving out of camera view
+    private void Teleport()
+    {
+        if (transform.position.x <= minXTeleportBound || transform.position.x >= maxXTeleportBound)
+        {
+            transform.position = new Vector2(-transform.position.x, transform.position.y);
+        }
+
+        else if (transform.position.y <= minYTeleportBound || transform.position.y >= maxYTeleportBound)
+        {
+            transform.position = new Vector2(transform.position.x, -transform.position.y);
+        }
+    }
+
+    // Countdown to destruction
+    private IEnumerator Terminate()
+    {
+        yield return new WaitForSeconds(destroyTime);
+        Destroy(gameObject.gameObject);
     }
 }
